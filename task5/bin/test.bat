ff53e41732c5fce162c0716bcd3e812fb7567bb4
RoadToExclusivity <dimaoq@gmail.com>
2015-02-26 21:42:52 +0300

    Executable files #5

---
+++ task5/bin/test.bat
@@ -0,0 +1,26 @@
+@echo off
+crypt.exe
+IF ERRORLEVEL 1 GOTO err
+
+crypt.exe crypt nofile.in nofile.out 225
+IF NOT ERRORLEVEL 1 GOTO err
+
+crypt.exe crypt 1.in 1.out string
+IF NOT ERRORLEVEL 1 GOTO err
+
+crypt.exe crypt 1.in 1.out 666
+IF NOT ERRORLEVEL 1 GOTO err
+
+crypt.exe crypt 1.in 1.out 44
+IF ERRORLEVEL 1 GOTO err
+crypt.exe decrypt 1.out 1_1.out 44
+IF ERRORLEVEL 1 GOTO err
+FC /B 1.in 1_1.out
+IF ERRORLEVEL 1 GOTO err
+
+ECHO Program testing succeeded :-)
+EXIT /B
+
+:err
+ECHO Program testing failed :-(
+EXIT /B
