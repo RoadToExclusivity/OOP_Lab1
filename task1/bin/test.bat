c2b216cbf5eec5ec8954f4e020c01166848571b7
Dmitry <dimaoq@gmail.com>
2015-03-18 09:54:44 +0300

    Добавлены тесты
    Исправлены баги программы

--- task1/bin/test.bat
+++ task1/bin/test.bat
@@ -5,22 +5,37 @@ IF ERRORLEVEL 1 GOTO err
 replace.exe no_such_file.txt output.txt awe ee
 IF NOT ERRORLEVEL 1 GOTO err
 
-replace.exe 1.in 1.in awe ee
+replace.exe 1.txt 1.txt awe ee
 IF NOT ERRORLEVEL 1 GOTO err
 
-replace.exe 1.in output.txt ff ""
+replace.exe 1.txt output.txt ff ""
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 1.out
-IF ERRORLEVEL 1 GOTO err
-
-replace.exe 2.in output.txt qw FTW
-IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 2.out
+FC /B output.txt 1_out.txt
 IF ERRORLEVEL 1 GOTO err
 
-replace.exe 3.in output.txt getc putc
+replace.exe 2.txt output.txt qw FTW
 IF ERRORLEVEL 1 GOTO err
-FC /B output.txt 3.out
+FC /B output.txt 2_out.txt
+IF ERRORLEVEL 1 GOTO err
+
+replace.exe 3.txt output.txt getc putc
+IF ERRORLEVEL 1 GOTO err
+FC /B output.txt 3_out.txt
+IF ERRORLEVEL 1 GOTO err
+
+replace.exe multiline_text.txt output.txt Hel HEL
+IF ERRORLEVEL 1 GOTO err
+FC /B output.txt multiline_text_out.txt
+IF ERRORLEVEL 1 GOTO err
+
+replace.exe recursive.txt output.txt 1231234 abcabcde
+IF ERRORLEVEL 1 GOTO err
+FC /B output.txt recursive_out.txt
+IF ERRORLEVEL 1 GOTO err
+
+replace.exe same_text.txt output.txt aa bb
+IF ERRORLEVEL 1 GOTO err
+FC /B output.txt same_text_out.txt
 IF ERRORLEVEL 1 GOTO err
 
 ECHO Program testing succeeded :-)
--- task1/src/LAB1_1/main.cpp
+++ task1/src/LAB1_1/main.cpp
@@ -1,15 +1,87 @@
-#include <cstdio>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int CalcCurrentPrefixFunctionValue(const string &pattern, 
+								   const vector<int> &patternPrefixFunction, 
+								   int prevPrefixFunctionValue, char c)
+{
+	int k = prevPrefixFunctionValue;
+	while (k > 0 && c != pattern[k])
+		k = patternPrefixFunction[k - 1];
+	if (c == pattern[k])
+		k++;
+
+	return k;
+}
+
+void CalcPrefixFunctionOnRange(const string &pattern, vector<int> &prefixFunction)
+{
+	int curPrefixFunctionValue = 0;
+	for (size_t i = 1; i < pattern.length(); i++)
+	{
+		curPrefixFunctionValue = CalcCurrentPrefixFunctionValue(pattern, prefixFunction, curPrefixFunctionValue, pattern[i]);
+		prefixFunction[i] = curPrefixFunctionValue;
+	}
+}
+
+void DoReplace(ifstream &fin, ofstream &fout, const string &searchString, 
+											  const string &replaceString)
+{
+	string pattern = searchString + '`', curLine = "", s = "";
+	int patternLength = pattern.length();
+	
+	vector<int> prefixFunction(patternLength);
+	CalcPrefixFunctionOnRange(pattern, prefixFunction);
+
+	int curPrefixFunctionValue, searchStringLength = searchString.length(), writingPos;
+	while (getline(fin, curLine))
+	{
+		curPrefixFunctionValue = prefixFunction[patternLength - 1];
+		writingPos = 0;
+		for (int i = 0; i < curLine.length(); i++)
+		{
+			curPrefixFunctionValue = CalcCurrentPrefixFunctionValue(pattern, prefixFunction, curPrefixFunctionValue, curLine[i]);
+			if (curPrefixFunctionValue == searchStringLength)
+			{
+				fout << replaceString;
+				writingPos = i + 1;
+				curPrefixFunctionValue = prefixFunction[patternLength - 1];
+			}
+			else
+			{
+				if (writingPos + searchStringLength <= i + 1)
+				{
+					fout << curLine[writingPos++];
+				}
+			}
+		}
+
+		if (writingPos < curLine.length())
+		{
+			for (size_t i = writingPos; i < curLine.length(); i++)
+			{
+				fout << curLine[i];
+			}
+		}
+		if (!fin.eof())
+			fout << endl;
+	}
+}
 
 int main(int argc, char *argv[]){
+
 	if (argc < 5)
 	{
 		printf("Not enough parameters\n");
 		return 0;
 	}
 
-	FILE *fin, *fout;
-	if (!(fin = fopen(argv[1], "r")))
+	ifstream fin(argv[1]);
+	if (!fin)
 	{
 		printf("Error with opening input file %s\n", argv[1]);
 		return 1;
@@ -18,59 +90,23 @@ int main(int argc, char *argv[]){
 	if (!strcmp(argv[1], argv[2]))
 	{
 		printf("Same file\n");
+		fin.close();
 		return 1;
 	}
 
-	if (!(fout = fopen(argv[2], "w")))
+	ofstream fout(argv[2]);
+	if (!fout)
 	{
 		printf("Error with opening output file %s\n", argv[2]);
+		fin.close();
 		return 1;
 	}
 	
+	string searchString = argv[3], replaceString = argv[4];
+	DoReplace(fin, fout, searchString, replaceString);
 
-	char *searchString = argv[3], *replaceString = argv[4];
-	int searchSLen = strlen(searchString), replaceSLen = strlen(replaceString), curPos = 0;
-	char *tempString = new char[replaceSLen + 1];
-	bool mbReplaceString = false;
-	int c;
-	while ((c = fgetc(fin)) != EOF)
-	{
-		if (replaceSLen && c == searchString[curPos])
-		{
-			if (!mbReplaceString)
-				mbReplaceString = true;
-			tempString[curPos] = c;
-			tempString[curPos + 1] = 0;
-			if (curPos == searchSLen - 1)
-			{
-				fputs(replaceString, fout);
-				curPos = 0;
-				mbReplaceString = false;
-			}
-			else
-			{
-				curPos++;
-			}
-		}
-		else
-		{
-			if (mbReplaceString)
-			{
-				fputs(tempString, fout);
-				curPos = 0;
-				mbReplaceString = false;
-			}
-			fputc(c, fout);
-		}
-	}
-	if (mbReplaceString)
-	{
-		fputs(tempString, fout);
-	}
-
-	fclose(fin);
-	fclose(fout);
-	delete[] tempString;
+	fin.close();
+	fout.close();
 	
 	return 0;
 }
